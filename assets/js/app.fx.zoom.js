b859540f19243c7570c995cefda99eac6f6347a6
Lyndel Thomas <lyndelt@gmail.com>
2014-12-06 19:04:00 -0800

    creating first example

--- assets/js/app.fx.zoom.js
+++ assets/js/app.fx.zoom.js
@@ -4,10 +4,30 @@
 
 	APP = APP || window.APP;
 
-	var View = APP.View.extend({
+	var Parent = APP.View;
 
+	APP.Views.Zoom = Parent.extend({
+
+		el: ".main",
+
+		options: {},
+		
+		events: {
+			"click .map": "zoomOut",
+			"click .stage": "zoomIn",
+		},
+
+		zoomOut: function( e ){
+			console.log("Hey 1");
+			$(e.target).addClass("fx-zoom-out").siblings().removeClass("fx-zoom-in");
+		},
+		
+		zoomIn: function( e ){
+			console.log("Hey 2");
+			$(e.target).addClass("fx-zoom-in").siblings().removeClass("fx-zoom-out");
+		}
 	});
 
-	APP.UI = APP.UI || {};
-	APP.UI.Component = View;
+	// APP.UI = APP.UI || {};
+	// APP.UI.Component = View;
 })( this.APP );
